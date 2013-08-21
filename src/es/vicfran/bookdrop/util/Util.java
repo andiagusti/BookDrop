c80b074067cee39c0801937a13a4bb7a9cc9a8b8
Victor de Francisco <vdefran@hi-iberia.es>
2013-08-21 14:24:44 +0200

    Add important comment to Util class

--- src/es/vicfran/bookdrop/util/Util.java
+++ src/es/vicfran/bookdrop/util/Util.java
@@ -1,8 +1,9 @@
 package es.vicfran.bookdrop.util;
 
 public class Util {
-	
+	// Be aware that it isn't a good way to store this info, 
+	// because it's sensitive data and it must be stored in a more
+	// secure way, for example obfuscated or encrypted.
 	public static final String APP_KEY = "46ed2n6l0rypk8t";
 	public static final String APP_SECRET = "quywwtzdqa9nyi0";
-
 }
