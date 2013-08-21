6ef68d244fe66945c9d5991a5715440f89f6de2f
Victor de Francisco <vdefran@hi-iberia.es>
2013-08-21 12:02:39 +0200

    Implement sign in functionality. Add app and secret keys from Dropbox app console

---
+++ src/es/vicfran/bookdrop/util/Util.java
@@ -0,0 +1,8 @@
+package es.vicfran.bookdrop.util;
+
+public class Util {
+	
+	public static final String APP_KEY = "46ed2n6l0rypk8t";
+	public static final String APP_SECRET = "quywwtzdqa9nyi0";
+
+}
