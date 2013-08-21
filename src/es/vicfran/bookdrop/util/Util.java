85ebf13464670ebadf743c1725401545e14f2b46
Victor de Francisco <victor_defran@yahoo.es>
2013-08-21 20:41:58 +0200

    For security reasons, refactor Util class to prevent against class instantiation. Add new app keys from Dropbox App console

--- src/es/vicfran/bookdrop/util/Util.java
+++ src/es/vicfran/bookdrop/util/Util.java
@@ -1,9 +1,25 @@
 package es.vicfran.bookdrop.util;
 
-public class Util {
+import android.content.Context;
+
+import com.dropbox.sync.android.DbxAccountManager;
+
+public final class Util {
+	// Private constructor to prevent against class instantiation
+	private Util() {}
+	
 	// Be aware that it isn't a good way to store this info, 
 	// because it's sensitive data and it must be stored in a more
 	// secure way, for example obfuscated or encrypted.
-	public static final String APP_KEY = "46ed2n6l0rypk8t";
-	public static final String APP_SECRET = "quywwtzdqa9nyi0";
+	//
+	// For more security, this data is private to prevent its use outside this class
+	private static final String APP_KEY = "8z1wqhkohai8aoz";
+	private static final String APP_SECRET = "ny1yoknc71nkwfn";
+	
+	// Folder of this app on Dropbox account
+	public static final String APP_FOLDER = "ebooks";
+	
+	public static DbxAccountManager getAccountManager(Context context) {
+		return DbxAccountManager.getInstance(context, APP_KEY, APP_SECRET);
+	}
 }
