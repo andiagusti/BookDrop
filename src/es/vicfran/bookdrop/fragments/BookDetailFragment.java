d61ecce31080c75d7ec2699496c562ab7fa9bc1d
Victor de Francisco <victor_defran@yahoo.es>
2013-08-25 17:11:47 +0200

    Implement book thumbnail creation

--- src/es/vicfran/bookdrop/fragments/BookDetailFragment.java
+++ src/es/vicfran/bookdrop/fragments/BookDetailFragment.java
@@ -1,23 +1,36 @@
 package es.vicfran.bookdrop.fragments;
 
+import java.io.IOException;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.dropbox.sync.android.DbxException;
+import com.dropbox.sync.android.DbxFile;
+import com.dropbox.sync.android.DbxFileSystem;
 import com.dropbox.sync.android.DbxPath;
 
 import es.vicfran.bookdrop.R;
+import es.vicfran.bookdrop.util.Util;
 
 public class BookDetailFragment extends Fragment {
 	
 	private TextView pathTextView;
+	private ImageView thumbnailImageView;
 	
 	public static String ARG_FILE_INFO = "es.vicfran.bookdrop.fragments:file_info";
 	
 	private DbxPath dbxPath;
+	private DbxFile thumb = null;
+	private Drawable thumbDrawable = null;
 	
 	public static BookDetailFragment buildBookDetailFragment(String path) {
 		BookDetailFragment bookDetailFragment = new BookDetailFragment();
@@ -33,18 +46,9 @@ public class BookDetailFragment extends Fragment {
 		return bookDetailFragment;
 	}
 	
-	// Official Android doc says that Fragments must have an empty constructor
-	public BookDetailFragment() {
-	}
-	
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-		return inflater.inflate(R.layout.fragment_book_detail, container, false);
-	}
-	
-	@Override
-	public void onViewCreated(View view, Bundle savedInstanceState) {
-		pathTextView = (TextView) view.findViewById(R.id.lbl_path);
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 		
 		Bundle args = getArguments();
 		if (args != null) {
@@ -55,8 +59,45 @@ public class BookDetailFragment extends Fragment {
 			}
 		}
 		
-		if (pathTextView != null) {
-			pathTextView.setText((dbxPath != null) ? dbxPath.toString() : "");
+		Context context = getActivity();
+		DbxFileSystem dbxFileSystem = Util.getFileSystem(getActivity());
+		
+		if (dbxFileSystem != null) {
+			try{
+				thumb = dbxFileSystem.openThumbnail(dbxPath, DbxFileSystem.ThumbSize.M, DbxFileSystem.ThumbFormat.PNG);
+			} catch (DbxException.NotFound e) {
+				Toast.makeText(context, context.getString(R.string.thumb_not_found_error), Toast.LENGTH_SHORT).show();
+			} catch (DbxException.InvalidParameter e) {
+				Toast.makeText(context, context.getString(R.string.thumb_not_file_error), Toast.LENGTH_SHORT).show();
+			} catch (DbxException.NoThumb e) {
+				Toast.makeText(context, context.getString(R.string.thumb_not_available_error), Toast.LENGTH_SHORT).show();
+			} catch (DbxException e) {
+				Toast.makeText(context, context.getString(R.string.thumb_error), Toast.LENGTH_SHORT).show();
+			}
+		}
+		
+		if ((thumb != null) && (thumb.isThumb())) {
+			try {
+				thumbDrawable = Drawable.createFromStream(thumb.getReadStream(), "");
+			} catch (IOException e) {
+				thumbDrawable = null;
+			}
+		}
+	}
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+		return inflater.inflate(R.layout.fragment_book_detail, container, false);
+	}
+	
+	@Override
+	public void onViewCreated(View view, Bundle savedInstanceState) {
+		pathTextView = (TextView) view.findViewById(R.id.lbl_path);
+		thumbnailImageView = (ImageView) view.findViewById(R.id.thumbnail);
+		
+		pathTextView.setText((dbxPath != null) ? dbxPath.toString() : "");
+		if (thumbDrawable != null) {
+			thumbnailImageView.setImageDrawable(thumbDrawable);
 		}
 	}
 }
